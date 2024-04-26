ca5dcda8b7c06863e126498dea72ba79f712448b
Kreal11 <kirillshyrokov2@gmail.com>
2024-04-26 14:26:50 +0300

    added resources for server and setGlobalPrefix

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { EstatesModule } from './estates/estates.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, EstatesModule],
   controllers: [AppController],
   providers: [AppService],
 })
